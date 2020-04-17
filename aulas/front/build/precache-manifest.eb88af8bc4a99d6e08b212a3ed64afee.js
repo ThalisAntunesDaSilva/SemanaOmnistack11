b0856690ef23d1a147fac2ae2d7bd8b3a0de95db
Thalis Antunes <thalisantunes@hotmail.com>
2020-04-17 14:27:37 -0300

    FrontEnd da aplicação

---
+++ aulas/front/build/precache-manifest.eb88af8bc4a99d6e08b212a3ed64afee.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a4423435a9908220d355e3b719df556",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c15f6e8f54d845e77291",
+    "url": "/static/js/2.116eabdc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.116eabdc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8f629b830441cf03825e",
+    "url": "/static/js/main.223c13be.chunk.js"
+  },
+  {
+    "revision": "055e94f6602794288cc3",
+    "url": "/static/js/runtime-main.1f1f19a3.js"
+  }
+]);
